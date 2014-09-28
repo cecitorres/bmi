309f537775de037b6ef2b7811cd85e3c9c430ab5
Luis Elizondo <lelizondo@gmail.com>
2014-09-28 01:54:08 -0500

    Rewrite find method in UserController to only return the current user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,20 @@
  */
 
 module.exports = {
-	
+	find: function(req, res) {
+		var user = req.user;
+
+		// make sure we load from the DB and not the session
+		var query = User.findOne(user.id)
+			.exec(function(err, result) {
+				if (err) {
+					sails.log.error(err);
+					return res.negotiate(err);
+				}
+				
+				res.status(200);
+				res.ok(result);
+			});
+	}
 };
 
